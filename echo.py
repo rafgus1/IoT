3aa92d3e0fa2119fe58b35af80cd4c4bc112ea61
rafgus1 <53905819+rafgus1@users.noreply.github.com>
2019-11-30 11:45:27 +0100

    Update echo.py

--- echo.py
+++ echo.py
@@ -3,11 +3,11 @@ import time
 GPIO.setmode(GPIO.BCM)
 TRIG = 23
 ECHO = 24
-print("Distance Measurement In Progress")
+print("Pomiar odległości")
 GPIO.setup(TRIG,GPIO.OUT)
 GPIO.setup(ECHO,GPIO.IN)
 GPIO.output(TRIG, False)
-print("Waiting For Sensor To Settle")
+print("Oczekiwanie na sensor")
 time.sleep(2)
 GPIO.output(TRIG, True)
 time.sleep(0.00001)
@@ -19,5 +19,5 @@ while GPIO.input(ECHO)==1:
 pulse_duration = pulse_end - pulse_start
 distance = pulse_duration*17150
 distance = round(distance, 2)
-print("Distance:",distance,"cm")
+print("Odległość:",distance,"cm")
 GPIO.cleanup()
